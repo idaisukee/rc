906364ac0bb5e69db50f49e288d0d5b2059db828
Daisuke Inoue <hibariyashiki@gmail.com>
2016-03-09 13:22:32 +0900

    amended display.rb to use @rc_(hour|min|sec).

--- bin/display.rb
+++ bin/display.rb
@@ -1,4 +1,4 @@
 require File.expand_path('../../lib/RevCal.rb', __FILE__)
 time = Time.now
 rc_time = RcTime.new(Time.now)
-print rc_time.rc_date_part.to_a.join('.'), ' ', (rc_time.rc_total_sec / (100 * 100)).round(2)
+print [rc_time.rc_date_part.to_a.join('.'), [rc_time.rc_hour, rc_time.rc_min].join('.')].join(' ')
