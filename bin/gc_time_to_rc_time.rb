42692da98b864abf2820a0c040ea89c803908bfc
Daisuke Inoue <hibariyashiki@gmail.com>
2016-05-19 14:24:27 +0900

    amended displaying.

--- bin/gc_time_to_rc_time.rb
+++ bin/gc_time_to_rc_time.rb
@@ -2,4 +2,4 @@ require File.expand_path('../../lib/RevCal.rb', __FILE__)
 input = STDIN.readline
 gc_time = Time.parse(input)
 rc_time = RcTime.new(gc_time)
-print ( rc_time.rc_total_sec / (100 * 100) ).round(2)
+print rc_time.rc_hour.to_s + '.' + rc_time.rc_min.to_s
